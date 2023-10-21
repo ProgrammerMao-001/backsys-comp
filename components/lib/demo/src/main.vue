ef5418f486cb0f0868539981cff11bc749bdf9cc
maoHangFei <3061148499@qq.com>
2023-10-21 12:52:44 +0800

    新建demo组件

---
+++ components/lib/demo/src/main.vue
@@ -0,0 +1,38 @@
+<!--
+ * @Description: main 页面
+ * @Author: mhf
+ * @Date: 2023/10/21
+-->
+<template>
+  <div>
+    <div class="a">
+      <div class="b">
+        this is DemoPage
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Demo",
+  components: {},
+  props: {},
+  computed: {},
+  watch: {},
+  filters: {},
+  data() {
+    return {};
+  },
+  methods: {},
+  created() {
+  },
+  mounted() {
+  },
+  destroyed() {
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
--- examples/main.js
+++ examples/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import "../components/css/index.scss"
+import Demo from "../components/lib/demo";
+Vue.use(Demo)
+
 Vue.config.productionTip = false
 
 new Vue({
