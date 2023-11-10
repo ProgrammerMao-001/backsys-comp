1aa8855ec523b11342c0a6ce29d1ec511e119a4c
maoHangFei <3061148499@qq.com>
2023-11-11 00:00:26 +0800

    ytTable

--- components/ytTable/src/main.vue
+++ components/ytTable/src/main.vue
@@ -3,31 +3,209 @@
  * @Author: mhf
  * @Date: 2023/11/10 22:44
 -->
+
+<!--:header-row-style="{-->
+<!--background: '#F7F8FA',-->
+<!--boxShadow: 'inset 0px -1px 0px 0px #EEEEEE',-->
+<!--fontFamily: 'MicrosoftYaHei-Bold, MicrosoftYaHei',-->
+<!--fontWeight: 'bold',-->
+<!--color: '#333333',-->
+<!--}"-->
+<!--:row-style="{-->
+<!--background: '#FFFFFF',-->
+<!--boxShadow: 'inset 0px -1px 0px 0px #EEEEEE',-->
+<!--fontFamily: 'MicrosoftYaHei',-->
+<!--color: '#333333',-->
+<!--}"-->
+
 <template>
-  <div class="ytTable">
-    ytTable
+  <div class="page">
+    <el-table
+        border
+        ref="tableRef"
+        v-loading="tableConfig.isLoading"
+        :height="height"
+        :data="tableData"
+        :row-key="handleRowKey"
+        @cell-click="handleCellClick"
+        @cell-dblclick="handleCellDbClick"
+        @selection-change="handleSelectionChange"
+    >
+      <template v-for="(item, index) in tableDataColumn">
+        <div :key="index">
+          <el-table-column
+              v-if="item.name === '序号'"
+              type="index"
+              :label="item.name"
+              :index="indexMethod"
+              fixed="left"
+          />
+          <el-table-column
+              v-else-if="item.name === '多选'"
+              :reserve-selection="true"
+              type="selection"
+          />
+          <el-table-column
+              v-else-if="item.name === '操作'"
+              :key="index"
+              label="操作"
+              align="right"
+              :width="btnWidth"
+              fixed="right"
+          >
+            <template slot-scope="scope">
+              <slot name="operationSlot" :row="scope.row"/>
+            </template>
+          </el-table-column>
+          <el-table-column
+              v-else
+              :key="index"
+              :label="item.name"
+              :prop="item.value"
+              :width="item.width"
+              :sortable="item.sortable"
+              show-overflow-tooltip
+          >
+            <template slot-scope="scope">
+              <div v-if="item.isSlot">
+                <slot :name="item.value" :row="scope.row"/>
+              </div>
+              <div v-else>{{ scope.row[item.value] }}</div>
+            </template>
+          </el-table-column>
+        </div>
+      </template>
+    </el-table>
   </div>
 </template>
 
 <script>
 export default {
   name: "ytTable",
-  components: {},
-  props: {},
-  computed: {},
-  watch: {},
-  filters: {},
-  data() {
-    return {};
+  props: {
+    btnWidth: {
+      type: String,
+      default: "240px",
+    }, // 操作栏宽度
+    tableData: {
+      type: Array,
+      default: () => {
+        return []
+      }
+    }, // 表格数据
+    tableDataColumn: {
+      type: Array,
+      default: () => {
+        return []
+      }
+    }, // 表头数据
+    tableConfig: {
+      type: Object,
+      default: () => {
+        return {}
+      }
+    }, // loading配置
+    height: {
+      type: String,
+      default: "45vh",
+    }, // 表格的高度
+    idName: {
+      type: String,
+      default: "id",
+    }, // 表格行的唯一标识
+    paginationConfig: {
+      type: Object,
+      default: () => {
+        return {
+          pageNum: 1,
+          pageSize: 10,
+        };
+      },
+    }, // 分页参数
   },
-  methods: {},
+  methods: {
+    /**
+     * @Event 方法
+     * @description: 删除操作
+     * */
+    handleDeleteData(row) {
+      this.$emit("handleDeleteData", row);
+    },
+
+    /**
+     * @Event 方法
+     * @description: 编辑操作
+     * */
+    handleUpdateData(row) {
+      this.$emit("handleUpdateData", row);
+    },
+
+    /**
+     * @Event 方法
+     * @description: 切换分页时，保持选中状态的标识
+     * */
+    handleRowKey(row) {
+      return row[this.idName];
+    },
+
+    /**
+     * @Event 监听多选操作
+     * @description:
+     * */
+    handleSelectionChange(val) {
+      this.$emit("handleSelectionChange", val);
+    },
+
+    /**
+     * @Event 当某个单元格被点击时会触发该事件
+     * @description:
+     * */
+    handleCellClick(val) {
+      console.log(val)
+    },
+
+    /**
+     * @Event 当某个单元格被双击时会触发该事件
+     * @description:
+     * */
+    handleCellDbClick(val) {
+      console.log(val)
+    },
+
+    /**
+     * @Event 方法
+     * @description: 表格 -> 取消选择
+     * */
+    handleResetSelection() {
+      this.$refs.tableRef.clearSelection();
+    },
+
+    /**
+     * @Event 方法
+     * @description: 设置表格后一页的index在前一页的index基础上累加
+     * */
+    indexMethod(index) {
+      // return index + 1 // 如果不想要累加效果，请打开此项
+      return (
+          index +
+          this.paginationConfig.pageSize * (this.paginationConfig.pageNum - 1) +
+          1
+      );
+    },
+  },
   created() {
-  },
-  mounted() {
-  },
-  destroyed() {
+    console.log(this.tableDataColumn)
   }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+::v-deep .el-table--border .el-table__cell {
+  border: none !important;
+}
+
+::v-deep .el-table--group,
+.el-table--border {
+  border: none !important;
+}
+</style>
--- examples/useYtTable/useYtTable.vue
+++ examples/useYtTable/useYtTable.vue
@@ -5,7 +5,34 @@
 -->
 <template>
   <div class="uesYtTable">
-    <ytTable></ytTable>
+    <ytTable
+        :table-data="tableData"
+        :table-config="tableConfig"
+        :table-data-column="tableDataColumn"
+        @handleSelectionChange="handleSelectionChange"
+    >
+      <template slot="type" slot-scope="scope">
+        <span> {{ scope.row.type | changeType }} </span>
+      </template>
+      <template slot="operationSlot" slot-scope="scope">
+        <el-button
+            type="text"
+            size="small"
+            icon="el-icon-edit"
+            @click="doSomething(scope.row, '详情')"
+        >详情
+        </el-button>
+        <el-button
+            type="text"
+            size="small"
+            icon="el-icon-edit"
+            @click="doSomething(scope.row, '修改')"
+        >修改
+        </el-button>
+
+      </template>
+    </ytTable>
+
   </div>
 </template>
 
@@ -16,12 +43,76 @@ export default {
   props: {},
   computed: {},
   watch: {},
-  filters: {},
-  data() {
-    return {};
+  filters: {
+    changeType(value) {
+      return value.replace("type", "类型")
+    }
   },
-  methods: {},
+  data() {
+    return {
+      tableData: [
+      ],
+      tableConfig: {
+        isLoading: false,
+      }, // 表格配置
+      tableDataColumn: [
+        { name: "序号", value: "index" },
+        { name: "多选", value: "select" },
+        { name: "名称", value: "typeName" },
+        { name: "描述", value: "description" },
+        { name: "类型", value: "type", isSlot: true },
+        { name: "操作", value: "operationSlot", isSlot: true },
+      ], // 表头数据
+      total: 0,
+      ids: [],
+    };
+  },
+  methods: {
+    handleSelectionChange(selection) {
+      this.ids = selection.map((item) => item.id);
+      console.log(this.ids)
+    },
+
+    doSomething(data, type) {
+      console.log(data, type)
+    }
+  },
   created() {
+    this.tableConfig.isLoading = true
+    setTimeout(() =>{
+      this.tableData = [
+        {
+          id: 1,
+          typeName: "名称1",
+          description: "描述1",
+          type: "type1"
+        },
+        {
+          id: 2,
+          typeName: "名称2",
+          description: "描述2",
+          type: "type2"
+        },
+        {
+          id: 3,
+          typeName: "名称3",
+          description: "描述3",
+          type: "type3"
+        },
+        {
+          id: 4,
+          typeName: "名称4",
+          description: "描述4",
+          type: "type4"
+        },
+        {
+          id: 5,
+          typeName: "名称5",
+          description: "描述5",
+          type: "type5"
+        },]
+      this.tableConfig.isLoading = false
+    }, 500)
   },
   mounted() {
   },
